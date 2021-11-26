b3d7a2c720a1bb047949f3dbd530e6bd3c74baaf
Paulino9699 <dapa7928@gmail.com>
2021-11-25 20:39:46 -0400

    Nuevos algortimos añadidos para guardar datos en el localStorage y poder extraerlo y mostrarlo al usuario 0.7

--- JS/app.js
+++ JS/app.js
@@ -12,6 +12,7 @@ let inputCursosBiblicos = document.querySelector(".input-cursosBiblicos");
 let inputPresentacionVideos = document.querySelector(".input-presentacionVideo");
 let inputAño = document.querySelector(".input-año");
 let inputMes = document.querySelector(".input-mes");
+let inputYearsFile = document.querySelector(".input-years-files");
 
 // TODOS LOS VALORES DE LOS INPUT
 let inputAñoValue = inputAño.value;
@@ -35,12 +36,16 @@ btnVentanaPopup.addEventListener('click', () => {
 addEventListener('DOMContentLoaded', function () {
     // Functión para agregar escucha btn de la lista para seleccionar los años de las carpetas
     listDeploy();
-    // Función para seleccionar un año para sacar los datos del localStorage correspodiente al año selecionado
-    selectionFileYears()
     // Función para guardar todos los datos en el localStorage
     btnSaveData();
     // Función para seleccionar el mes mediante algoritmo
     seleccionMes();
+    // Guardar el año seleccionado en el localStorage para que siempre se quede guardado y cuando el usuario entre siempre va a aparecer puesto en el input de archivos de los años
+    saveFullYearsLocalStorage()
+    // Función para seleccionar un año para sacar los datos del localStorage correspodiente al año selecionado
+    selectionFileYears();
+    // Funcion para extraer archivos del localStorage solo si hay un año seleccionado
+    showFolders();
 });
 
 
@@ -62,22 +67,6 @@ function seleccionMes() {
     }
 }
 
-// Aqui lo que se hace es que despues que se haga click en cualquiera de los meses que se encuentra en la lista, el valor del input va acambiar a un numero correspondiente a su numero de mes esto sin alterar el valor del input ya seleccionado y ya cargado al propio input que en este caso el valor del input serí el nombre del mes.
-function convertirMes() {
-    let inputMesValue = inputMes.value;
-    if (inputMesValue == 'Enero') inputMesValue = 1;
-    if (inputMesValue == 'Febrero') inputMesValue = 2;
-    if (inputMesValue == 'Marzo') inputMesValue = 3;
-    if (inputMesValue == 'Abril') inputMesValue = 4;
-    if (inputMesValue == 'Mayo') inputMesValue = 5;
-    if (inputMesValue == 'Junio') inputMesValue = 6;
-    if (inputMesValue == 'Julio') inputMesValue = 7;
-    if (inputMesValue == 'Agosto') inputMesValue = 8;
-    if (inputMesValue == 'Septiembre') inputMesValue = 9;
-    if (inputMesValue == 'Octubre') inputMesValue = 10;
-    if (inputMesValue == 'Noviembre') inputMesValue = 11;
-    if (inputMesValue == 'Diciembre') inputMesValue = 12;
-};
 
 // Desplegar lista de los meses al hacer click en el indicador tipo flecha abajo
 document.querySelector(".content-btn-listaMes").addEventListener('click', () => {
@@ -85,6 +74,7 @@ document.querySelector(".content-btn-listaMes").addEventListener('click', () => 
     document.querySelector(".svg-listaMes").classList.toggle('rotate');
 })
 
+// Función para alertar al usuario que debe de llenar algunos input correctamente
 function evaluarCampoAño() {
     if (inputAño.value === '') {
         textAlertPopup.innerHTML = `¡El campo "año" está vacío! ${'<br>'} Debe de introducir un año.`;
@@ -101,6 +91,7 @@ function evaluarCampoAño() {
     }
 }
 
+// Función para guardar datos en el localStorage al presionar ek botón "guardar"
 function btnSaveData() {
     btnGuardar.addEventListener("click", function () {
         guardarDatosLocalStorage();
@@ -114,6 +105,7 @@ function btnSaveData() {
     });
 }
 
+// Función con algoritmo para conectarse al localStorage y guardar datos
 function guardarDatosLocalStorage() {
 
     if (inputMes.value != '' && (inputAño.value >= 2021 && inputAño.value <= 2023)) {
@@ -208,9 +200,7 @@ function guardarDatosLocalStorage() {
     }
 }
 
-let datos = localStorage.getItem('PresentacionVideos (11/2021)');
-
-
+// Función para desplegar lista al dar click en el boton de la flechita
 function listDeploy() {
     let fileYears = document.querySelector(".list-fileYears");
     let btnListDeploy = document.querySelector(".btn-list-deploy");
@@ -219,66 +209,142 @@ function listDeploy() {
     });
 };
 
+// Función para seleccionar un año y ponerlo en localStorage
 function selectionFileYears() {
+    // Dejar puesto el año al cargar el documento de cero
+    let datosLocalStorage = localStorage.getItem('selectionFullYears');
+    document.querySelector('.input-years-files').value = datosLocalStorage;
+
+    let fileYears = document.querySelector(".list-fileYears").children;
+    for (let i = 0; i < fileYears.length; i++) {
+        fileYears[i].addEventListener('click', function () {
+            let datosLocalStorage = localStorage.getItem('selectionFullYears');
+            document.querySelector('.input-years-files').value = datosLocalStorage;
+        });
+    }
+}
+
+// Función para guardar el año selecionado en el localStorage
+function saveFullYearsLocalStorage() {
     let fileYears = document.querySelector(".list-fileYears").children;
     for (let i = 0; i < fileYears.length; i++) {
         fileYears[i].addEventListener('click', function () {
             let fileYearsChildrens = fileYears[i].childNodes[0].nodeValue;
-            document.querySelector('.input-years-files').value = fileYearsChildrens;
+            if (localStorage.getItem('selectionFullYears') === null) {
+                localStorage.setItem('selectionFullYears', fileYearsChildrens)
+            } else {
+                let datosLocalStorage = localStorage.getItem('selectionFullYears');
+                datosLocalStorage = fileYearsChildrens;
+                localStorage.setItem('selectionFullYears', datosLocalStorage);
+            }
         });
     }
+
 }
-showFolders()
+
+
+// Funcion para extraer archivos del localStorage solo si hay un año seleccionado y mostrarlo al usuario
 function showFolders() {
 
-    // Extraer los datos del INPUT MES en formato de numeros, correspondientes a su respectivo mes.
-    let inputYearsFilesVALUE = document.querySelector(".input-years-files").value;
+    let datosLocalStorageHoras = localStorage.getItem(`Horas (Diciembre/2021)`);
+    let datosLocalStorageRevisitas = localStorage.getItem(`Revisitas (Diciembre/2021)`);
+    let datosLocalStorageComentarios = localStorage.getItem(`Comentarios (Diciembre/2021)`);
+    let datosLocalStoragePublicaciones = localStorage.getItem(`Publicaciones (Diciembre/2021)`);
+    let datosLocalStorageCursosBiblicos = localStorage.getItem(`CursosBiblicos (Diciembre/2021)`);
+    let datosLocalStoragePresentacionVideos = localStorage.getItem(`PresentacionVideos (Diciembre/2021)`);
 
+    let fileYears = document.querySelector(".list-fileYears").children;
+    for (let i = 0; i < fileYears.length; i++) {
+        fileYears[i].addEventListener('click', function () {
+            filesYears2021();
+        });
+    }
+    // Abrir carpetas de todos los meses del año 2021
+    filesYears2021();
 
-    let datosLocalStorageHoras = localStorage.getItem(`Horas (${inputMes.value}/${2021})`);
-    let datosLocalStorageRevisitas = localStorage.getItem(`Revisitas (${inputMes.value}/${2021})`);
-    let datosLocalStorageComentarios = localStorage.getItem(`Comentarios (${inputMes.value}/${2021})`);
-    let datosLocalStoragePublicaciones = localStorage.getItem(`Publicaciones (${inputMes.value}/${2021})`);
-    let datosLocalStorageCursosBiblicos = localStorage.getItem(`CursosBiblicos (${inputMes.value}/${2021})`);
-    let datosLocalStoragePresentacionVideos = localStorage.getItem(`PresentacionVideos (${inputMes.value}/${2021})`);
+    function filesYears2021() {
+        if (inputYearsFile.value == '2021') {
 
+            // Si en el localStoragese encuentra esa clave entonces se va a crear una carpeta de enero del 2021 y se va a mostrar al usuario
+            if (localStorage.getItem('Horas (Diciembre/2021)') != null) {
+                // console.log(localStorage.getItem(`Horas (Enero/2021)`))
 
-    var datosHora = '';
+                // ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ HORAS ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦
+                if (localStorage.getItem('Horas (Diciembre/2021)') != null) {
+                    var datosHora = `<div><h2>Horas:</h2><h1>
+                                                            ${datosLocalStorageHoras}
+                                                        </h1></div>`;
+                }
+                // ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ REVISITAS ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦
+                if (localStorage.getItem('Revisitas (Diciembre/2021)') != null) {
+                    var datosRevisitas = `<div><h2>Revisitas:</h2><h1>
+                                                                    ${datosLocalStorageRevisitas}
+                                                                </h1></div>`;
+                }
+                // ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ COMENTARIOS ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦
+                if (localStorage.getItem('Comentarios (Diciembre/2021)') != null) {
+                    var datosComentarios = `<div><h2>Comentarios:</h2><h1>
+                                                                        ${datosLocalStorageComentarios}
+                                                                    </h1></div>`;
+                }
+                // ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ PUBLICACIONES ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦
+                if (localStorage.getItem('Publicaciones (Diciembre/2021)') != null) {
+                    var datosPublicaciones = `<div><h2>Publicaciones:</h2><h1>
+                                                                            ${datosLocalStoragePublicaciones}
+                                                                        </h1></div>`;
+                }
+                // ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ CURSOS ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦
+                if (localStorage.getItem('CursosBiblicos (Diciembre/2021)') != null) {
+                    var datosCursosBiblicos = `<div><h2>Cursos Bíblicos:</h2><h1>
+                                                                                ${datosLocalStorageCursosBiblicos}
+                                                                            </h1></div>`;
+                }
+                // ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦ PRESENTACION ¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦
+                if (localStorage.getItem('PresentacionVideos (Diciembre/2021)') != null) {
+                    var datosPresentacionVideos = `<div><h2>Presentación Vídeos:</h2><h1>
+                                                                                ${datosLocalStoragePresentacionVideos}
+                                                                            </h1></div>`;
+                }
 
-    btnGuardar.addEventListener('click', function () {
-        // console.log(localStorage.getItem(`Horas (${'Enero'}/${2021})`))
-        if (localStorage.getItem(`Horas (${'Enero'}/${2021})`) != null) {
-            datosHora = `<div><h2>Horas:</h2><h1>20</h1></div>`;
+                let contenedorResultadoDelMes = document.querySelector(".container-foldersMonths");
+                contenedorResultadoDelMes.innerHTML = `<div>
+                                                            <h3>Diciembre</h3>
+                                                            <div>
+                                                                ${datosHora}
+                                                                ${datosRevisitas}
+                                                                ${datosComentarios}
+                                                                ${datosPublicaciones}
+                                                                ${datosCursosBiblicos}
+                                                                ${datosPresentacionVideos}
+                                                            </div>
+                                                        </div>`;
+            }
         }
-    });
-    console.log(datosHora)
-
-    // console.log(localStorage.getItem(`Horas (${inputMes.value}/${2021})`));
-
-
-
-
-    let contenedorResultadoDelMes = document.querySelector(".container-foldersMonths");
-
-    contenedorResultadoDelMes.innerHTML += ``;
-    contenedorResultadoDelMes.innerHTML += `<div>
-                                            <h3>Enero</h3>
-                                            <div>
-                                                ${datosHora}
-                                                <div>
-                                                    <h2>Horas:</h2>
-                                                    <h1>20</h1>
-                                                </div>
-                                            </div>
-                                        </div>`;
-
-
-
+    }
+    // console.log(localStorage.getItem('Comentarios (Diciembre/2021)'));
 
 }
 
 
 
+
+
+// // Aqui lo que se hace es que despues que se haga click en cualquiera de los meses que se encuentra en la lista, el valor del input va acambiar a un numero correspondiente a su numero de mes esto sin alterar el valor del input ya seleccionado y ya cargado al propio input que en este caso el valor del input serí el nombre del mes.
+// function convertirMes() {
+//     let inputMesValue = inputMes.value;
+//     if (inputMesValue == 'Enero') inputMesValue = 1;
+//     if (inputMesValue == 'Febrero') inputMesValue = 2;
+//     if (inputMesValue == 'Marzo') inputMesValue = 3;
+//     if (inputMesValue == 'Abril') inputMesValue = 4;
+//     if (inputMesValue == 'Mayo') inputMesValue = 5;
+//     if (inputMesValue == 'Junio') inputMesValue = 6;
+//     if (inputMesValue == 'Julio') inputMesValue = 7;
+//     if (inputMesValue == 'Agosto') inputMesValue = 8;
+//     if (inputMesValue == 'Septiembre') inputMesValue = 9;
+//     if (inputMesValue == 'Octubre') inputMesValue = 10;
+//     if (inputMesValue == 'Noviembre') inputMesValue = 11;
+//     if (inputMesValue == 'Diciembre') inputMesValue = 12;
+// };
 
 
 
